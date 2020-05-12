d347e93f31a1e7862bbdc00f30b4c710cf72e2ea
nabikhalid <khalid18@my.yorku.ca>
2020-05-12 02:31:40 -0400

    variadic

--- variadic functions/variadic.go
+++ variadic functions/variadic.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+// take an arbitrary number of arguements as inputs, println is a variadic function
+
+func multiply(nums ...int) int { // nums is the input
+	total := 1
+
+	for _, num := range nums {
+		total *= num // total = total * num
+	}
+
+	return total
+}
+
 func main() {
+
+	fmt.Println("this", "is", "a", "variadic function")
+
+	// call multiply
+
+	fmt.Println(multiply(1, 2, 3, 4))
+
+	// can be applied to slices
+
+	nums := []int{1, 2, 3, 4}
+
+	fmt.Println(multiply(nums...)) // don't forget ...
 
 }
