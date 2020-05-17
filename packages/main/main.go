684a4c8bfb05c9e2478b0238c04f0b9f9436ac1b
nabikhalid <khalid18@my.yorku.ca>
2020-05-17 02:06:53 -0400

    calm

--- packages/main/main.go
+++ packages/main/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nabikhalid/GO-workspace/packages/mymath"
+)
 
 func main() {
 	fmt.Println("Golazo.")
-
+	x := 1
+	y := 1
+	mymath.AddDat(x, y)
 }
