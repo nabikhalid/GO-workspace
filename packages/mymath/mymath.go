2556a235b9b67d528d1341000327133f88af54e2
nabikhalid <khalid18@my.yorku.ca>
2020-05-17 01:57:15 -0400

    re-attempting packages

---
+++ packages/mymath/mymath.go
@@ -0,0 +1,8 @@
+package math
+
+// function name is capitalized, thus exportable
+
+// AddDat adds two integers together
+func AddDat(x int, y int) int {
+	return x + y
+}
--- packages/other/mymath.go
+++
@@ -1,5 +0,0 @@
-package mymath
-
-func add(x int, y int) int {
-	return x + y
-}
