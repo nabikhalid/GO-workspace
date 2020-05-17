d0c2c5469b18a7dbefb48f070234038e497e6ec7
nabikhalid <khalid18@my.yorku.ca>
2020-05-17 02:22:42 -0400

    pppackages

--- packages/main/main.go
+++ packages/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/nabikhalid/GO-workspace/packages/mymath"
+	math "github.com/nabikhalid/GO-workspace/packages/mymath"
 )
 
 func main() {
 	fmt.Println("Golazo.")
 	x := 1
 	y := 1
-	mymath.AddDat(x, y)
+	fmt.Println(math.AddDat(x, y))
+
+	//
 }
