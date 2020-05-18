2226a9f8df97f60e5e2139888547fa4285eacde1
nabikhalid <khalid18@my.yorku.ca>
2020-05-18 03:17:12 -0400

    packages done

--- packages/main.go
+++ packages/main.go
@@ -6,13 +6,14 @@ import (
 	"github.com/nabikhalid/GO-workspace/packages/mymath"
 )
 
-// all this time the answer was -u
-// go get -u "github.com/..."
+// HOW TO IMPORT PACKAGES IN GO:
+// go get -u "github.com/..." in the folder containing main, wow.
 
 func main() {
 
 	fmt.Println("yessirski")
 	fmt.Println(mymath.Add(1, 2))
+	fmt.Println(mymath.Yessirski())
 
 	// lowercase doesn't work
 	// fmt.Println(mymath.subtract(1, 2))
